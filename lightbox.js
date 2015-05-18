1e4a3a23cfa7d166473348af6bac8bb40a138163
Travis Gorkin <travis.gorkin@gmail.com>
2015-05-18 01:20:31 -0700

    Add transition animations to lighbox image paging, move logic out of embedded scripts into lightbox.js.

--- lightbox.js
+++ lightbox.js
@@ -1,6 +1,8 @@
 var imageUrls =[];
 var currentImgIdx = -1;
 
+var isShowing = false;
+
 exports.setImages = function(images) {
   imageUrls = images;
   currentImgIdx = -1;
@@ -8,11 +10,32 @@ exports.setImages = function(images) {
 
 exports.showImage = function(imgUrl) {
   currentImgIdx = imageUrls.indexOf(imgUrl);
+
+  var img = $('<img id="activeImage" class="lightboxImage"/>').attr("src", imgUrl);
+  $('#lightboxContent').append(img);
+
+  if( !isShowing ) {
+    $('#lightbox').fadeIn(600);
+    $("#pageFrame").addClass("blur");
+    isShowing = true;
+  }
+}
+
+exports.hide = function() {
+  isShowing = false;
+  $(".lightboxImage").remove();
+  $("#pageFrame").removeClass("blur");
+  $('#lightbox').hide();
 }
 
 exports.nextImage = function() {
   currentImgIdx = (currentImgIdx + 1) % imageUrls.length;
-  return imageUrls[currentImgIdx];
+  var imgUrl = imageUrls[currentImgIdx];
+
+  var oldImage = $("#activeImage").removeAttr("id");
+  var newImage = createImage(imgUrl);
+
+  playImageTransition(oldImage, newImage, -1);
 }
 
 exports.prevImage = function() {
@@ -21,5 +44,31 @@ exports.prevImage = function() {
   } else {
     currentImgIdx--;
   }
-  return imageUrls[currentImgIdx];
+  var imgUrl = imageUrls[currentImgIdx];
+
+  var oldImage = $("#activeImage").removeAttr("id");
+  var newImage = createImage(imgUrl);
+
+  playImageTransition(oldImage, newImage, 1);
+}
+
+function playImageTransition(oldImage, newImage, xDirection) {
+  var targetX = xDirection > 0 ? 2000 : -2000;
+
+  oldImage.animate(
+    { left: targetX },
+    400, "easeInQuad",
+    function() { oldImage.remove(); }
+  )
+
+  newImage.css('left', -targetX).animate(
+    { left: 0 },
+    400, "easeInQuad"
+  );
+}
+
+function createImage(imgUrl) {
+  var img = $('<img id="activeImage" class="lightboxImage"/>').attr("src", imgUrl);
+  $('#lightboxContent').append(img);
+  return img;
 }
